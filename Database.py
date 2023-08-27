081f4126f380bbbc379d8355be6b3697fa0cbc64
lapiceroazul4 <lapiceroazul@proton.me>
2023-08-27 09:56:06 -0500

    updating database.py, in order to make a data warehouse

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 import json 
 import pandas as pd
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date, Text
+from sqlalchemy import create_engine, Column, Integer, Float, String, Boolean, Date, Text, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -16,43 +16,22 @@ db_url = f"mysql+pymysql://{usuario}:{password}@{server}/{database}"
 Base = declarative_base()
 
 
-class Airbnb(Base):
+class Airbnbs(Base):
+    __tablename__ = 'Airbnb_Transactions'
+    airbnb_id = Column(Integer, ForeignKey('airbnb_detail.id'))
+    host_id = Column(Integer, ForeignKey('host_table.host_id'))
+    neighbourhood_id = Column(Integer, ForeignKey('neighbourhood_table.neighbourhood_id'))
 
-    __tablename__ = "Airbnb_Data"
-    airbnb_id = Column("airbnb_id", Integer, primary_key=True)
-    name = Column("name", String(150))
-    host_id = Column("host_id", Integer)
-    host_identity_verified = Column("host_identity_verified", Boolean)
-    host_name = Column("host_name", String(50))
-    neighbourhood_group = Column("neighbourhood_group", String(50))
-    neighbourhood = Column("neighbourhood", String(50))
-    latitude = Column("latitude", Integer)
-    longitude = Column("longitude", Integer)
-    country = Column("country", String(20))
-    country_code = Column("country_code", String(3))
-    instant_bookable  = Column("instant_bookable ", Boolean)
-    cancellation_policy = Column("cancellation_policy", String(10))
-    room_type  = Column("room_type ", String(20))
-    construction_year = Column("construction_year ", Date)
-    price = Column("price", Integer)
-    service_fee = Column("service_fee", Integer)
-    minimum_night = Column("minimum_night", Integer)
-    number_of_reviews = Column("number_of_reviews", Integer)
-    last_review = Column("last_review", Date)
-    reviews_per_month = Column("reviews_per_month ", Integer)
-    reviews_rate_number = Column("reviews_rate_number", Integer)
-    availability_365 = Column("availability_365", Integer)
-    house_rules = Column("house_rules", Text) 
-
-    def __init__ (self, session):
+    def __init__ (self, session, engine):
         self.session = session
+        Base.metadata.create_all(engine)
 
-    #Create a new Airbnb, new_airbnb should bring all the information preferably in a dictionary.
+    #Create a new transaction, new_airbnb should bring all the information preferably in a dictionary.
     def create(self, new_airbnb):
         self.session.add(new_airbnb)
         self.session.commit()
     
-    #Update an Airbnb, update_data must bring all the information to update
+    #Update an Airbnb Transaction, update_data must bring all the information to update
     def update(self, update_data):
         for key, value in update_data.items():
             setattr(self, key, value)
@@ -61,6 +40,102 @@ class Airbnb(Base):
     #Delete rows, requires Airbnb ID
     def delete(self, airbnb_id):
         self.session.delete(airbnb_id)
+        self.session.commit()
+
+class Hosts(Base):
+
+    __tablename__ = 'host_table'
+
+    host_id = Column(Integer, primary_key=True)
+    host_name = Column(String)
+    host_identity_verified = Column(Boolean)
+
+    def __init__ (self, session, engine):
+        self.session = session
+        Base.metadata.create_all(engine)
+
+    #Create a new Host, new_hostshould bring all the information preferably in a dictionary.
+    def create(self, new_host):
+        self.session.add(new_host)
+        self.session.commit()
+    
+    #Update an Airbnb, update_data must bring all the information to update
+    def update(self, update_data):
+        for key, value in update_data.items():
+            setattr(self, key, value)
+        self.session.commit()
+
+    #Delete rows, requires Host ID
+    def delete(self, host_id):
+        self.session.delete(host_id)
+        self.session.commit()
+
+class Airbnb_Details(Base):
+    __tablename__ = 'airbnb_detail'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    instant_bookable = Column(String)
+    cancellation_policy = Column(String)
+    roomtype = Column(String)
+    construction_year = Column(Integer)
+    price = Column(Float)
+    service_fee = Column(Float)
+    minimum_nights = Column(Integer)
+    number_of_reviews = Column(Integer)
+    last_review = Column(String)
+    reviews_per_month = Column(Float)
+    review_rate_number = Column(Float)
+    calculated_host_listings_count = Column(Integer)
+    availability_365 = Column(Integer)
+    house_rules = Column(String)
+
+    def __init__ (self, session, engine):
+        self.session = session
+        Base.metadata.create_all(engine)
+
+    #Create a new Airbnb_Detail, Airbnb_Detail should bring all the information preferably in a dictionary.
+    def create(self, Airbnb_Detail):
+        self.session.add(Airbnb_Detail)
+        self.session.commit()
+    
+    #Update an Airbnb Detail, Airbnb_Detail must bring all the information to update
+    def update(self, update_data):
+        for key, value in update_data.items():
+            setattr(self, key, value)
+        self.session.commit()
+
+    #Delete rows, requires Airbnb ID
+    def delete(self, id):
+        self.session.delete(id)
+        self.session.commit()
+
+class Neighbourhoods(Base):
+    __tablename__ = 'neighbourhoods'
+
+    neighbourhood_id = Column(Integer, primary_key=True)
+    neighbourhood_group = Column(String)
+    neighbourhood = Column(String)
+    lat = Column(Float)
+    long = Column(Float)
+
+    def __init__ (self, session, engine):
+        self.session = session
+        Base.metadata.create_all(engine)
+
+    #Create a new Neighbor, Neighbor should bring all the information preferably in a dictionary.
+    def create(self, Neighbor):
+        self.session.add(Neighbor)
+        self.session.commit()
+    
+    #Update an Neighbor, update_Neighbor must bring all the information to update
+    def update(self, update_Neighbor):
+        for key, value in update_Neighbor.items():
+            setattr(self, key, value)
+        self.session.commit()
+
+    #Delete rows, requires Neighbor_id
+    def delete(self, neighbourhood_id):
+        self.session.delete(neighbourhood_id)
         self.session.commit()
 
 #Function to Create Engine
@@ -77,7 +152,3 @@ def creating_session(engine):
 #Function to close the session
 def closing_session(session):
     session.close()
-
-
-
-

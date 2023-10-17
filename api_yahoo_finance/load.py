5ebe5324794666d5d1a36de81ee479f5975194b8
ImBetterThanYesterday <99687072+ImBetterThanYesterday@users.noreply.github.com>
2023-10-17 01:29:44 -0500

    documetation update

--- api_yahoo_finance/load.py
+++ api_yahoo_finance/load.py
@@ -11,57 +11,61 @@ db_params = {
     "password": "mysecretpass",   # Contraseña del usuario
 }
 
-def transform():
+def call_to_transform():
     df = read_Data()
     df_previo = cambiar_a_columna_date(df)
     df_final = borrar_columns(df_previo)
     print(df_final)
     return df_final
 
-# Intentar conectarse a la base de datos
-try:
-    connection = psycopg2.connect(**db_params)
-    cursor = connection.cursor()
+def load_data():
+    # Intentar conectarse a la base de datos
+    try:
+        connection = psycopg2.connect(**db_params)
+        cursor = connection.cursor()
 
-    # Ejecutar consultas aquí
-    cursor.execute("SELECT version();")
-    version = cursor.fetchone()
-    print("Conexión exitosa a PostgreSQL:", version)
+        # Ejecutar consultas aquí
+        cursor.execute("SELECT version();")
+        version = cursor.fetchone()
+        print("Conexión exitosa a PostgreSQL:", version)
 
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS apiyahoofinace(
-    Date TIMESTAMP,
-    Open DOUBLE PRECISION,
-    High DOUBLE PRECISION,
-    Low DOUBLE PRECISION,
-    Close DOUBLE PRECISION,
-    Volume INTEGER
-    );
-    """
-    cursor.execute(create_table_query)
-    connection.commit()
+        create_table_query = """
+        CREATE TABLE IF NOT EXISTS apiyahoofinace(
+        Date TIMESTAMP,
+        Open DOUBLE PRECISION,
+        High DOUBLE PRECISION,
+        Low DOUBLE PRECISION,
+        Close DOUBLE PRECISION,
+        Volume INTEGER
+        );
+        """
+        cursor.execute(create_table_query)
+        connection.commit()
 
-    # Cargar los datos desde el DataFrame en la tabla
-    engine = create_engine('postgresql://postgres:mysecretpass@localhost:5435/postgres')
-    df_final = transform()  # Supongo que aquí obtienes tu DataFrame final
-    print(df_final.columns)
-    df_final.to_sql('apiyahoofinace', con=engine, if_exists='append', index=False)
+        # Cargar los datos desde el DataFrame en la tabla
+        engine = create_engine('postgresql://postgres:mysecretpass@localhost:5435/postgres')
+        df_final = call_to_transform()  # Supongo que aquí obtienes tu DataFrame final
+        print(df_final.columns)
+        df_final.to_sql('apiyahoofinace', con=engine, if_exists='append', index=False)
 
-    # Consultar el catálogo information_schema para obtener las tablas
-    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
-    tables = cursor.fetchall()
+        # Consultar el catálogo information_schema para obtener las tablas
+        cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+        tables = cursor.fetchall()
 
-    # Imprimir las tablas
-    print("Tablas en la base de datos:")
-    for table in tables:
-        print(table[0])
+        # Imprimir las tablas
+        print("Tablas en la base de datos:")
+        for table in tables:
+            print(table[0])
 
-    # Confirmar los cambios
-    connection.commit()
+        # Confirmar los cambios
+        connection.commit()
 
-    # Cerrar la conexión
-    cursor.close()
-    connection.close()
+        # Cerrar la conexión
+        cursor.close()
+        connection.close()
 
-except Exception as e:
-    print("Error de conexión:", e)
+    except Exception as e:
+        print("Error de conexión:", e)
+
+if __name__ =="__main__":
+    load_data()
